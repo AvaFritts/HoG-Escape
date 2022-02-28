e4bfbcc326e0c5f09d33545eebb8047871d19a22
AvaFritts <avanfritts@gmail.com>
2022-02-28 17:34:00 -0600

    Zooms work Now

--- HeartoG-Escape-Unity/Assets/Scripts/CameraNavigation.cs
+++ HeartoG-Escape-Unity/Assets/Scripts/CameraNavigation.cs
@@ -16,22 +16,32 @@ using UnityEngine.UI;
 public class CameraNavigation : MonoBehaviour
 {
     /**VARIABLES**/
+    static public GameObject POI;
+
+
     [Header("Set in Inspector")]
-    public float rotat = 0; //current rotation of the system
-    //Vector3 rot = new Vector3(0, 0, 0);
+    [Tooltip("The cameras used for zoom-ins and normal viewing")]
     public GameObject myCam; //The event camera
     public GameObject mainCam; //The Main Camera
+    public Vector3 homePos; //= (4.05, 0.6, 0);
+    [Space(10)]
+    [Tooltip("The Button associated with turning left")]
+    public GameObject leftButton;
+    public GameObject rightButton;
+    public GameObject downButton;
+
 
 
     //The idea: Whenever one of the two buttons are pressed, the camera rotates to show another part of the room.
     //Any object not on the selected wall gets hidden
 
     //A second camera will be used to help with the zoom-ins.
+    //It might be nice to throw away the need for a second script and to check for tags instead... but I doubt I could do that so easily.
 
     //The forseen problem is the inventory. UI is only on one view. There are 3 ways to solve this:
-     //1: The UI is duplicated on each screen. This is going to be complicated and require a LOT of debugging.
-     //2: Have the UI for the Inventory change with each screen. Could work... Idk.
-     //3: Two cameras on the UI? Maybe.
+    //1: The UI is duplicated on each screen. This is going to be complicated and require a LOT of debugging.
+    //2: Have the UI for the Inventory change with each screen. Could work... Idk.
+    //3: Two cameras on the UI? Maybe.
 
     // Start is called before the first frame update
     void Start()
@@ -40,16 +50,14 @@ public class CameraNavigation : MonoBehaviour
         //Quaternion rotation = Quaternion.Euler(rot);
 
         myCam.SetActive(false);
-        if (rotat < -270 || rotat > 270) //just so I don't have to worry about extreme values.
-        {
-            rotat = 0;
-        }
+        downButton.SetActive(false);
+        
     }
 
     // Update is called once per frame
     public void OnLeftClick()
     {
-        this.transform.Rotate(0, -90, 0);  
+        this.transform.Rotate(0, -90, 0);  //rotating at a -90.
     }
 
     public void OnRightClick()
@@ -59,18 +67,33 @@ public class CameraNavigation : MonoBehaviour
 
     public void OnDownClick()
     {
+        //Go back to previous main view.
+        POI = null; //If this line isn't here, the camera will keep swapping back to the last zoom.
+        mainCam.SetActive(true);
         myCam.SetActive(false);
-        mainCam.SetActive(true);//rotat + 90;
+        //Change the UI accordingly
+        rightButton.SetActive(true);
+        leftButton.SetActive(true);
+        downButton.SetActive(false); //the button is not needed when the camera is zoomed out.
+
     }
 
-   /** private void FixedUpdate()
+    public void FixedUpdate()
     {
-        if (rotat < -270 || rotat > 270) //just so I don't have to worry about extreme values.
-        {
-            rotat = 0;
-            //myCam.transform.position.x = eventPos.x;
-            myCam.transform.Rotate(0, rotat, 0);
-        }
-    }**/
+        if (POI == null) return;
+        //Swap the camera to the zoom-in camera
+        mainCam.SetActive(false);
+        myCam.SetActive(true);
+
+        //move the Zoom-in Camera to the desired position
+        Vector3 destination = POI.transform.position; 
+        myCam.transform.position = destination;
+        myCam.transform.rotation = POI.transform.rotation;
+
+        //Activate the correct UI buttons
+        rightButton.SetActive(false);
+        leftButton.SetActive(false);
+        downButton.SetActive(true);
+    }
 
 }
---
+++ HeartoG-Escape-Unity/Assets/Scripts/ZoomPosition.cs
@@ -0,0 +1,61 @@
+/**** 
+ * Created by: Ava Fritts
+ * Date Created: Feb 28, 2022
+ * 
+ * Last Edited by: NA
+ * Last Edited: Feb 28, 2022
+ * 
+ * Description: The script for each zoom in... Probably never needed this to begin with.
+****/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomPosition : MonoBehaviour
+{
+    //[Header("Set in Inspector?")]
+    //public GameObject theObject;
+    //public float fOV;
+   
+    /**Variables**/
+    [Header("Set dynamically")]
+    //public bool canGet; It turns out that this Boolean is never needed in the first place.
+    public GameObject theObject;
+    Vector3 evPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Vector3 evPos = this.GetComponent<Transform>;
+    }
+
+    public void OnMouseEnter()
+    {
+        print("Mouse found " + theObject); //checking to see that the mouse can click it.
+    }
+    public void OnMouseExit()
+    {
+        print("Mouse left " + theObject);
+ 
+    }
+
+    // Update is called once per frame
+    /**  void FixedUpdate()
+      {
+          if (POI == null) return;
+          Vector3 destination = POI.transform.position;
+
+          transform.position = destination;
+      }**/
+
+    public void OnMouseDown()
+    {
+        //if (canGet == true)
+        //{
+            print(theObject.transform.position);
+            //CameraNavigation.eFOV = fOV;
+            CameraNavigation.POI = theObject;
+
+        //}
+    }
+}
