38519f1d4d523551392c3f4128e771b9d1452ce5
AvaFritts <avanfritts@gmail.com>
2022-03-10 18:08:56 -0600

    Removed Text Mesh and fixed Bugs

--- HeartoG-Escape-Unity/Assets/Scripts/PuzzleBehavior.cs
+++ HeartoG-Escape-Unity/Assets/Scripts/PuzzleBehavior.cs
@@ -2,8 +2,7 @@
  * Created by: Ava Fritts
  * Date Created: March 3, 2022
  * 
- * Last Edited by: NA
- * Last Edited: March 7, 2022
+ * Last Edited: March 8, 2022
  * 
  * Description: The system that controls the main puzzle dynamics.
 ****/
@@ -15,20 +14,23 @@ public class PuzzleBehavior : MonoBehaviour
 {
 
     /**VARIABLES**/
+    [Header("Set in Inspector")]
     public GameObject lockedObj;
 
     public GameObject submitButton;
     public GameObject[] puzzleButtons;
 
-    private Renderer mr; //for changing color;
+    private Renderer currentColor; //for changing color;
 
-    public string currentID;
+    [Space(10)]
     public string targetSolution; //the string
+    [Header("Set Dynamically")]
+    public string currentID; //the answer the player submitted.
 
     // Start is called before the first frame update
     void Start()
     {
-        mr = GetComponent<Renderer>();
+        currentColor = GetComponent<Renderer>();
     }
 
     // Update is called once per frame
@@ -37,18 +39,19 @@ public class PuzzleBehavior : MonoBehaviour
         print("Submitting thing");
        foreach(GameObject targetButton in puzzleButtons)
         {
-            currentID += targetButton.GetComponent<PuzzleSprites>().currentValue.ToString(); //making the 
+            currentID += targetButton.GetComponent<PuzzleSprites>().currentValue.ToString(); //making a string out of each puzzle piece.
         }
-        print(currentID);
+        //print(currentID);
         if (currentID.Equals(targetSolution)){
             if (gameObject.transform.parent.tag.Equals("C")) //if this is the door for the courage ending
             {
                 lockedObj.GetComponent<Lock>().Unlock();
+                currentColor.material.color = new Color(0f, 1f, 0f, 1f); //Changes the color of the button to green
             }
             else
             {
-                gameObject.transform.parent.transform.parent.GetComponent<Lock>().Unlock();
-                mr.material.color = new Color(0f, 1f, 0f, 1f);
+                gameObject.transform.parent.transform.parent.GetComponent<Lock>().Unlock(); //unlocks the door
+                currentColor.material.color = new Color(0f, 1f, 0f, 1f); //changes the color of the button to green
             }
         }
         currentID = null;
